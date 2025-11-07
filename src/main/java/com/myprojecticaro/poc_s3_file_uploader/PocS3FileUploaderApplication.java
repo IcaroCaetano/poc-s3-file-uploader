7baa9e1967b85ee45bf61c3f83dece17f3dabfdf
IcaroCaetano <icaro.caetano@gmail.com>
2025-11-07 09:50:51 -0300

    Initial commit: AWS S3 File Upload POC

---
+++ src/main/java/com/myprojecticaro/poc_s3_file_uploader/PocS3FileUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.myprojecticaro.poc_s3_file_uploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocS3FileUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocS3FileUploaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myprojecticaro/poc_s3_file_uploader/PocS3FileUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojecticaro.poc_s3_file_uploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocS3FileUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
