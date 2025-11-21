3b4a74a9ceffa79db87e9b964459fca1b37b0af6
Icaro Caetano <35819169+IcaroCaetano@users.noreply.github.com>
2025-11-20 22:19:21 -0300

    Update ZipService.java

--- src/main/java/com/myprojecticaro/poc_s3_file_uploader/service/ZipService.java
+++ src/main/java/com/myprojecticaro/poc_s3_file_uploader/service/ZipService.java
@@ -1,1 +1,39 @@
+package com.myprojecticaro.poc_s3_file_uploader.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+/**
+ * Service responsible for compressing multiple uploaded files into a single ZIP file.
+ */
+@Service
+public class ZipService {
+
+    /**
+     * Compresses the provided MultipartFile array into a single in-memory ZIP file.
+     *
+     * @param files array of files to be zipped
+     * @return byte array representing the compressed ZIP file
+     * @throws IOException if an error occurs while reading the files or creating the ZIP
+     */
+    public byte[] zipFiles(MultipartFile[] files) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ZipOutputStream zipOut = new ZipOutputStream(baos);
+
+        for (MultipartFile file : files) {
+            ZipEntry zipEntry = new ZipEntry(file.getOriginalFilename());
+            zipOut.putNextEntry(zipEntry);
+            zipOut.write(file.getBytes());
+            zipOut.closeEntry();
+        }
+
+        zipOut.close();
+        return baos.toByteArray();
+    }
+}
 
